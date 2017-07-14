6074638adc59241f343a397a8b39faf0613d31bb
Leo Picado <leopic@gmail.com>
2017-07-13 21:19:55 -0600

    Ejemplo sencillo de anidacion

--- App.js
+++ App.js
@@ -1,50 +1,116 @@
+'use strict';
+
 import React from 'react';
 import {
     AppRegistry,
-    View,
     Button,
+    Image,
+    StyleSheet,
     Text,
+    View,
 } from 'react-native';
 
-import { StackNavigator } from 'react-navigation';
+import { DrawerNavigator } from "react-navigation";
 import { TabNavigator } from "react-navigation";
-
-
-class HomeScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Welcome',
-    };
-    render() {
-        const { navigate } = this.props.navigation;
-
-        return <View>
-            <Text>Hola!</Text>
-            <Button onPress = {() => navigate('Main')} title = "Main screen" />
-        </View>;
-    }
-}
 
 class RecentChatsScreen extends React.Component {
     render() {
-        return <Text>List of recent chats</Text>
+        return <View style={{padding: 40}}>
+            <Text>List of recent chats</Text>
+            <Button onPress={() => this.props.navigation.navigate('DrawerOpen')}
+                    title="Open"/>
+        </View>
     }
 }
 
 class AllContactsScreen extends React.Component {
     render() {
-        return <Text>List of all contacts</Text>
+        return <View style={{padding: 40}}>
+            <Text>List of all contacts</Text>
+            <Button onPress={() => this.props.navigation.navigate('DrawerOpen')}
+                    title="Open"/>
+        </View>
     }
 }
 
 const MainScreenNavigator = TabNavigator({
-    Recent: { screen: RecentChatsScreen },
-    All: { screen: AllContactsScreen },
+    Recent: {screen: RecentChatsScreen},
+    All: {screen: AllContactsScreen},
 });
 
+class MyHomeScreen extends React.Component {
+    static navigationOptions = {
+        drawerLabel: 'Home',
+        drawerIcon: ({tintColor}) => (
+            <Image
+                source={require('./chats-icon.png')}
+                style={[styles.icon, {tintColor: tintColor}]}
+            />
+        ),
+    };
 
-const SimpleApp = StackNavigator({
-    Home: { screen: HomeScreen },
-    Main: { screen: MainScreenNavigator},
+    render() {
+        return (
+            <View style={[styles.container]}>
+                <Text>Home</Text>
+                <Button onPress={() => this.props.navigation.navigate('DrawerOpen')}
+                        title="Open"/>
+            </View>
+        );
+    }
+}
+
+class MyNotificationsScreen extends React.Component {
+    static navigationOptions = {
+        drawerLabel: 'Notifications',
+        drawerIcon: ({tintColor}) => (
+            <Image
+                source={require('./notif-icon.png')}
+                style={[styles.icon, {tintColor: tintColor}]}
+            />
+        ),
+    };
+
+    render() {
+        return (
+            <View style={[styles.container]}>
+                <Text>Notifications</Text>
+                <Button onPress={() => this.props.navigation.navigate('DrawerOpen')}
+                        title="Open"/>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 24,
+        height: 24,
+    },
+    container: {
+      padding: 40,
+    },
 });
 
-AppRegistry.registerComponent('NavegacionesAnidadas', () => SimpleApp);
+const MyApp = DrawerNavigator({
+    Home: {
+        screen: MyHomeScreen,
+    },
+    Notifications: {
+        screen: MyNotificationsScreen,
+    },
+    Main: {
+        screen: MainScreenNavigator,
+        navigationOptions: {
+            drawerLabel: 'Main',
+            drawerIcon: ({tintColor}) => (
+                <Image
+                    source={require('./chats-icon.png')}
+                    style={[styles.icon, {tintColor: tintColor}]}
+                />
+            ),
+        },
+    },
+});
+
+AppRegistry.registerComponent('NavegacionesAnidadas', () => MyApp);
